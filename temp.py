ce5d2efaf4eb2c1d1993be5e9a1c4ac4393e4566
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-19 15:39:17 -0600

    Make funtion

--- temp.py
+++ temp.py
@@ -1,3 +1,10 @@
 # Program to convert celsius to Fahrenheit
-fah = 25 * 9/5 + 32
-print(f"25 degrees is {fah} Fahrenheit")
+def cal_fah(cel):
+    fah = cel * 9/5 + 32
+    return fah
+    
+user = float(input("Enter Celcius: "))
+
+fahrenheit = cal_fah(user)
+
+print(f"25 degrees is {fahrenheit} Fahrenheit")
