20a1161163e71b640305d69a4429c41627661d37
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-19 16:03:21 -0600

    Added celcius function

--- temp.py
+++ temp.py
@@ -1,10 +1,19 @@
 # Program to convert celsius to Fahrenheit
-def calc_fah(cel):
+def calc_fahrenheit(cel):
     fah = cel * 9/5 + 32
     return fah
 
+def calc_celcius(fah):
+    cel = 5/9   * (fah - 32)
+    return cel
 
-celcius = float(input("Enter Celcius: "))
-fahrenheit = calc_fah(celcius)
 
-print(f"25 degrees is {fahrenheit} Fahrenheit")
+
+
+user = float(input("Enter a Celcius: "))
+
+fahrenheit = calc_fahrenheit(user)
+celcius = calc_celcius(fahrenheit)
+
+print(f"The {user} degrees celcius is {fahrenheit} Fahrenheit")
+print(f"The {fahrenheit} degrees fahrenheit is {celcius} celcius")
