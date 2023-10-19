c3ebc5c13bba2f52978c6d06a3b62898b9905063
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-19 15:49:43 -0600

    Change formatting

--- temp.py
+++ temp.py
@@ -2,9 +2,8 @@
 def calc_fah(cel):
     fah = cel * 9/5 + 32
     return fah
-    
-user = float(input("Enter Celcius: "))
 
-fahrenheit = calc_fah(user)
+celcius = float(input("Enter Celcius: "))
+fahrenheit = calc_fah(celcius)
 
 print(f"25 degrees is {fahrenheit} Fahrenheit")
